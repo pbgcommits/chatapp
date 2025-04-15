ded74633739c46a5ebe19c83fa382d23dfd3f59d
Patrick Barton Grace <132808103+Tuthar@users.noreply.github.com>
2025-04-15 13:26:32 +1000

    Fixed unit tests

--- chatapp_test.go
+++ chatapp_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net"
+	"strings"
 	"testing"
 	"time"
 )
@@ -25,11 +27,12 @@ func serverConnect(t *testing.T) (connection net.Conn) {
 }
 
 func chooseUsername(t *testing.T, connection net.Conn, username string) string {
-	usernameRequest := make([]byte, 0)
+	usernameRequest := make([]byte, 1024)
 	_, err := connection.Read(usernameRequest)
 	if err != nil {
 		t.Errorf("Failed to get username request: %v", err.Error())
 	}
+	fmt.Printf("From server: %s", usernameRequest)
 	_, err = connection.Write([]byte(username))
 	if err != nil {
 		t.Errorf("Failed to send username request: %v", err.Error())
@@ -38,12 +41,19 @@ func chooseUsername(t *testing.T, connection net.Conn, username string) string {
 }
 
 func choosePassword(t *testing.T, connection net.Conn, password string) string {
-	passwordRequest := make([]byte, 0)
-	for i := range 2 {
-		i += 0 // idk why it complains about the underscore
+	// The server sometimes sends its password request in a single message
+	// and sometimes in multiple, hence the strange check for the last message
+	for {
+		passwordRequest := make([]byte, 1024)
 		_, err := connection.Read(passwordRequest)
 		if err != nil {
 			t.Errorf("Failed to get password request: %v", err.Error())
+		}
+		splitBySpaces := strings.Split(string(passwordRequest), " ")
+		// -2, as the last word is apparently a bunch of empty space
+		fmt.Printf("From server: %s\n", passwordRequest)
+		if splitBySpaces[len(splitBySpaces)-2] == "password:" {
+			break
 		}
 	}
 	_, err := connection.Write([]byte(password))
@@ -55,25 +65,21 @@ func choosePassword(t *testing.T, connection net.Conn, password string) string {
 
 func TestServerConnection(t *testing.T) {
 	activateServer()
-	connection := serverConnect(t)
-	connection.Close()
+	_ = serverConnect(t)
 }
 
 func TestUsername(t *testing.T) {
 	activateServer()
 	connection := serverConnect(t)
 	chooseUsername(t, connection, "1")
-	connection.Close()
 }
 
 func TestPassword(t *testing.T) {
 	activateServer()
 	connection1 := serverConnect(t)
-	username1 := chooseUsername(t, connection1, "1")
+	username1 := chooseUsername(t, connection1, "2")
 	choosePassword(t, connection1, username1)
 	connection2 := serverConnect(t)
-	username2 := chooseUsername(t, connection2, "2")
+	username2 := chooseUsername(t, connection2, "3")
 	choosePassword(t, connection2, username2)
-	connection1.Close()
-	connection2.Close()
 }
